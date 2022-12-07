294ee5b5f25296559b7177e8802cf6503b0a27c5
Fadoua <fadelhannachi@gmail.com>
2022-12-07 21:48:56 +0100

    creating views

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -4,3 +4,12 @@ from django.shortcuts import render
 # Create your views here.
 def hello(request):
     return HttpResponse('<h1>Hello Django!</h1>')
+
+def about(request):
+    return HttpResponse('<h1>A propos</h1> <p>Nous aimons merch!</p>')
+
+def listings(request):
+    return HttpResponse('<h2> Liste des annonces pour les articles</h2>')
+
+def contactus(request):
+    return HttpResponse('<p>Contactez nous ici</p>')
