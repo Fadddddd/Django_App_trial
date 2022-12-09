378d4f52c7f6668cc267d96ee99eee81bc6d9599
Fadoua <fadelhannachi@gmail.com>
2022-12-09 10:37:14 +0100

    creating views

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -1,15 +1,35 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from listings.models import Band
+from listings.models import Listing
 
 # Create your views here.
 def hello(request):
-    return HttpResponse('<h1>Hello Django!</h1>')
+    bands=Band.objects.all()
+    return HttpResponse(f"""
+    <h1>Hello Django!</h1>
+    <p>My favorites groups are: </p>
+    <ul>
+    <li>{bands[0].name}</li>
+    <li>{bands[1].name}</li>
+    <li>{bands[2].name}</li>
+    </ul>
+ """)
 
 def about(request):
     return HttpResponse('<h1>A propos</h1> <p>Nous aimons merch!</p>')
 
 def listings(request):
-    return HttpResponse('<h2> Liste des annonces pour les articles</h2>')
+    listings=Listing.objects.all()
+    return HttpResponse(f"""
+    <h2> List of the concerts :</h2>
+    <p>Coming soon...</p>
+    <ul>
+    <li>{listings[0].title}</li>
+    <li>{listings[1].title}</li>
+    <li>{listings[2].title}</li>
+    </ul>
+    """)
 
 def contactus(request):
     return HttpResponse('<p>Contactez nous ici</p>')
